b403b15976f7fd76d875bb80597b1e5edaa4aaff
EduGese <122921699+EduGese@users.noreply.github.com>
2024-08-21 19:45:37 +0200

    First exercise: dinamic_title

--- Exercises/dinamic_title/src/App.jsx
+++ Exercises/dinamic_title/src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let word = '';
+ const [title, setTitle] = useState('Inserta aqui tu titulo');
+ 
+ 
+ const handleTitleChange = () => {
+    setTitle(word);
+  };
+  const handleDeleteTitle = () => {
+    setTitle('Inserta aqui tu titulo');
+  };
+  
+  const getLetters = (event)=>{
+     word = event.target.value;
+  }
+  const clearInput = () => {
+    document.querySelector('input').value = '';
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{title}</h1>
+    
+      <label>Inserta aqui tu titulo</label>
+      <input type="text" onKeyUp={getLetters}/>
+
+      <button onClick={clearInput}>Clear</button>
+      <br /><br /><br /><br /><br /><br />
+      <button onClick={handleTitleChange} className="change-title">Insertar titulo</button>
+      <button onClick={handleDeleteTitle}>Borrar titulo</button>
     </>
   )
 }
